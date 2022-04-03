c2ddfd1c98f46c691d614fba3dc04f873e3d31dc
Uriel Kelman <uriel.k@lemon.me>
2022-04-03 02:51:42 -0300

    Starting the app

--- decentraveller/App.tsx
+++ decentraveller/App.tsx
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
+import { Dimensions } from 'react-native';
+import Home from "./home/Home";
+import Owned from "./owned/Owned";
+import { AntDesign } from "@expo/vector-icons";
+import {sizesConfig} from "./config/sizesConfig";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Decentraveller!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+type DeviceDimensions = {
+    width: number;
+    height: number;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+type RootStackScreens = {
+  Home: undefined;
+  Owned: undefined;
+};
+
+type bottomTabElementProps = {
+    focused: boolean;
+    color: string;
+    size: number;
+};
+
+const deviceDimensions: DeviceDimensions = {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+}
+
+const Tab = createBottomTabNavigator<RootStackScreens>();
+
+const getTabBarIcon = (
+  route: Readonly<{ key: string; name: "Home" | "Owned"; path?: string }>,
+  iconSize: number,
+) => {
+  return ({ focused, color, size }: bottomTabElementProps): JSX.Element => {
+    if (route.name === "Home") {
+      return <AntDesign name="home" size={iconSize} color="black" />;
+    } else if (route.name === "Owned") {
+      return <AntDesign name="user" size={iconSize} color="black" />;
+    }
+    return <AntDesign name="home" size={iconSize} color="black" />;
+  };
+};
+
+const getBottomTabNavigatorScreenOptions = (
+  route: Readonly<{ key: string; name: "Home" | "Owned"; path?: string }>
+): BottomTabNavigationOptions => {
+  return {
+    tabBarIcon: getTabBarIcon(route, deviceDimensions.width / sizesConfig.bottomTabIconSizeWidthInverseFraction),
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+      tabBarStyle: {
+        height: deviceDimensions.height / sizesConfig.bottomTabHeightInverseFraction,
+      },
+      tabBarLabelStyle: {
+        fontSize: deviceDimensions.width / sizesConfig.bottomTabFontSizeWidthInverseFraction,
+          fontFamily: 'sans-serif-medium',
+          fontWeight: 'bold'
+      }
+  };
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        backBehavior="initialRoute"
+        screenOptions={({ route }) => getBottomTabNavigatorScreenOptions(route)}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Owned" component={Owned} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
