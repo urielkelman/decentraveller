fda3a44fc526747e6d46eac3c4abc85bc1c72c66
Uriel Kelman <uriel.k@lemon.me>
2022-04-03 02:51:42 -0300

    Starting the app

---
+++ decentraveller/config/sizesConfig.ts
@@ -0,0 +1,11 @@
+export type SizesConfig = {
+    bottomTabHeightInverseFraction: number;
+    bottomTabIconSizeWidthInverseFraction: number;
+    bottomTabFontSizeWidthInverseFraction: number;
+}
+
+export const sizesConfig: SizesConfig = {
+    bottomTabHeightInverseFraction: 16,
+    bottomTabIconSizeWidthInverseFraction: 14,
+    bottomTabFontSizeWidthInverseFraction: 36,
+}
---
+++ decentraveller/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Home: React.FC = () => {
+  return (
+    <View>
+      <Text>I´m the home component. Here you can search different places.</Text>
+    </View>
+  );
+};
+
+export default Home;
