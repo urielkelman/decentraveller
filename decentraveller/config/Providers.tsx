5ac6ff3375f2f71185fa282babac7ee1217c00a7
Uriel Kelman <uriel.k@lemon.me>
2022-09-06 10:27:58 -0300

    provider

--- decentraveller/config/Providers.tsx
+++ decentraveller/config/Providers.tsx
@@ -1,5 +1,17 @@
-/*const Providers: React.FC = () => {
+import { MoralisProvider } from "react-moralis";
+import Moralis from 'moralis/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+// @ts-ignore
+import { MORALIS_APP_ID, MORALIS_SERVER_URL } from "react-native-dotenv";
+
+Moralis.setAsyncStorage(AsyncStorage)
+
+const Providers: React.FC = ({ children}) => {
     return (
-        <Moral
-    )
-}*/
+        <MoralisProvider serverUrl={MORALIS_SERVER_URL} appId={MORALIS_APP_ID} environment='native'>
+            {children}
+        </MoralisProvider>
+        )
+}
+
+export default Providers;
