f52d751e42c710de152645565bc0a45364093c92
Uriel Kelman <uriel.k@lemon.me>
2022-09-17 20:54:20 -0300

    a ver ahora

---
+++ decentraveller-app/screens/home/Home.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button, Text, View } from "react-native";
+import {useWalletConnect} from "@walletconnect/react-native-dapp";
+
+const Home = () => {
+    const connector = useWalletConnect();
+
+    const connectWallet = React.useCallback(() => {
+        return connector.connect();
+    }, [connector]);
+
+    const killSession = React.useCallback(() => {
+        return connector.killSession();
+    }, [connector]);
+
+    console.log(connector)
+
+    return (
+        <View>
+            {!connector.connected && (
+                <View>
+                    <Button title={"Login"} onPress={connectWallet} />
+                </View>
+            )}
+            {connector.connected && (
+                <View>
+                    <Button title={"Logout"} onPress={killSession} />
+                    <Text>{connector.accounts[0]}</Text>
+                </View>
+            )}
+
+        </View>
+    );
+};
+
+export default Home;
