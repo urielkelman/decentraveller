dd13edb010b0c226eb867b68d5103a0cd0f6ed37
Uriel Kelman <uriel.k@lemon.me>
2022-10-26 01:28:14 -0300

    added a lot of config and finished first deployment script

--- decentraveller-blockchain/scripts/deploy.ts
+++ decentraveller-blockchain/scripts/deploy.ts
@@ -1,4 +1,24 @@
-import { ethers } from "hardhat";
+import { ethers, run, network } from "hardhat";
+
+const verify = async (contractAddress: string, args: any[]) => {
+    console.log("Doing contract verification....");
+
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (e: unknown) {
+        if (
+            e instanceof Error &&
+            e.message.toLowerCase().includes("already verified")
+        ) {
+            console.log("Already verified");
+        } else {
+            console.log(e);
+        }
+    }
+};
 
 const main = async () => {
     const DecentravellerFactory = await ethers.getContractFactory(
@@ -8,6 +28,32 @@ const main = async () => {
     const decentraveller = await DecentravellerFactory.deploy();
     await decentraveller.deployed();
     console.log(`Deployed contract address ${decentraveller.address}`);
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        // We wait for a few blocks to be mined so the scanner can index the transaction.
+        await decentraveller.deployTransaction.wait(5);
+        await verify(decentraveller.address, []);
+    }
+
+    const transactionResponse = await decentraveller.addPlace(
+        "Shami Shawarma",
+        0,
+        "33.46",
+        "-54.35"
+    );
+
+    await transactionResponse.wait(1);
+
+    console.log("Review hash is: ", transactionResponse.hash);
+
+    const addReviewTx = decentraveller.addReview(
+        "Shami Shawarma",
+        "El mejor lugar para comer shawarma"
+    );
+
+    const shamiShawarmaPlace = await decentraveller.getReviews(
+        "Shami Shawarma"
+    );
+    console.log(shamiShawarmaPlace);
 };
 
 main()
