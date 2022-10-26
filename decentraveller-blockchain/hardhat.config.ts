4787deb5eb9d7e59d0b8515ac60fbf50c3b73640
Uriel Kelman <uriel.k@lemon.me>
2022-10-26 01:28:14 -0300

    added a lot of config and finished first deployment script

--- decentraveller-blockchain/hardhat.config.ts
+++ decentraveller-blockchain/hardhat.config.ts
@@ -1,8 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL as string;
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY as string;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
