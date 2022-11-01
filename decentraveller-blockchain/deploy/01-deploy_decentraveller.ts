bf6182a625ac26093c155f9b8db44dbbdf0e0a5e
Uriel Kelman <uriel.k@lemon.me>
2022-11-01 02:24:13 -0300

    hardhat deploy plugin configu

---
+++ decentraveller-blockchain/deploy/01-deploy_decentraveller.ts
@@ -0,0 +1,14 @@
+import { network } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+export default async (hre: HardhatRuntimeEnvironment) => {
+    const { getNamedAccounts, deployments } = hre;
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+
+    const decentraveller = deploy("Decentraveller", {
+        from: deployer,
+        log: true,
+    });
+};
