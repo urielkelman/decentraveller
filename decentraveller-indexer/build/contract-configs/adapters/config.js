8a6faaacb29996c403e52b19b822ee5a598a792a
Uriel Kelman <uriel.k@lemon.me>
2023-02-07 01:11:11 -0300

    remove unnecesay folder

--- decentraveller-indexer/build/contract-configs/adapters/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.eventEndpoints = void 0;
-exports.eventEndpoints = {
-    NEW_PLACE_ENDPOINT: "api/places",
-};
