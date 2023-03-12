075abbbe872f712054d3859d50414a0dec5936ad
Uriel Kelman <uriel.k@lemon.me>
2023-03-12 17:21:08 -0300

    full flow

--- decentraveller-indexer/src/contract-configs/transformers/NewPlaceTransformer.ts
+++ decentraveller-indexer/src/contract-configs/transformers/NewPlaceTransformer.ts
@@ -5,17 +5,32 @@ import EventTransformer from "./EventTransformer";
 
 class NewPlaceTransformer extends EventTransformer {
     public transformEvent(event: any[]): NewPlaceRequest {
+        console.log(event);
         return {
             endpoint: eventEndpoints.NEW_PLACE_ENDPOINT,
             body: {
                 id: (event[0] as BigNumber).toNumber(),
                 placeCreator: event[1],
                 placeName: event[2],
-                tourismField: event[3],
-                latitude: event[4],
-                longitude: event[5],
+                physicalAddress: event[3],
+                category: this.categoryNumberToString(event[4]),
+                latitude: event[5],
+                longitude: event[6],
             },
         };
+    }
+
+    private categoryNumberToString(category: number): string {
+        switch (category) {
+            case 0:
+                return "GASTRONOMY";
+            case 1:
+                return "ACCOMODATION";
+            case 2:
+                return "ENTERTAINMENT";
+            default:
+                return "UNKNOWN";
+        }
     }
 }
 
