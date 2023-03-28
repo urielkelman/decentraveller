11f532b055fad7383befb9d9349e68481a2e59f8
Gianmarco Cafferata <giancafferata@hotmail.com>
2023-03-27 22:02:43 -0300

    ETL script

--- decentraveller-blockchain/README.md
+++ decentraveller-blockchain/README.md
@@ -11,3 +11,5 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.ts
 ```
+
+Yelp dataset URL: https://www.kaggle.com/datasets/yelp-dataset/yelp-dataset
--- decentraveller-blockchain/scripts/create_place.ts
+++ decentraveller-blockchain/scripts/create_place.ts
@@ -23,6 +23,6 @@ const main = async () => {
 main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error();
+        console.error(error);
         process.exit(1);
     });
