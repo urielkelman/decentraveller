aaa75499334353038fd95213e1a8b7af3596515f
Uriel Kelman <uriel.k@lemon.me>
2023-04-07 21:52:38 -0300

    working

---
+++ decentraveller-blockchain/scripts/create_place.ts
@@ -0,0 +1,32 @@
+import { ethers, run, network } from "hardhat";
+import { Decentraveller } from "../typechain-types";
+
+const main = async () => {
+    const decentravellerContract: Decentraveller = await ethers.getContractAt(
+        "Decentraveller",
+        "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
+    );
+
+    try {
+        const result = await decentravellerContract.addPlace(
+            "Shami shawarma",
+            "25.3232",
+            "23.321",
+            "Sanfe Fe 3173",
+            0
+        );
+
+        await result.wait(1);
+
+        console.log(result);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error();
+        process.exit(1);
+    });
