016ddf1fd67a2eb09a72a4617a04b0c4577a8d36
Gianmarco Cafferata <giancafferata@hotmail.com>
2023-05-02 23:30:52 -0300

    Geocoding when creating place (#10)
    * Geocoding when creating place
    
    * Removed env file
    
    * Forward geocoding in separate endpoint
    
    * Removed .env from config docker

--- decentraveller-api/web-api/src/orms/place.py
+++ decentraveller-api/web-api/src/orms/place.py
@@ -12,10 +12,9 @@ class PlaceORM(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    address = Column(String, nullable=True)
+    address = Column(String, nullable=False)
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
-    open_hours = Column(JSON, nullable=True)
     categories = Column(String, nullable=True)
     sub_categories = Column(String, nullable=True)
     review = relationship("ReviewORM", uselist=False)
