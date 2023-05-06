74e8f2a5cb7dd82d041a326ad75911393df8c94a
Gianmarco Cafferata <giancafferata@hotmail.com>
2023-05-06 15:35:43 -0300

    User profiles

--- decentraveller-blockchain/contracts/Decentraveller.sol
+++ decentraveller-blockchain/contracts/Decentraveller.sol
@@ -7,20 +7,57 @@ import "./DecentravellerPlaceCloneFactory.sol";
 import "hardhat/console.sol";
 
 error Place__NonExistent(uint256 placeId);
+error Profile__NicknameInUse(string nickname);
+error Address__Unregistered(address sender);
 
 contract Decentraveller {
-    event NewProfile(
+    event ProfileUpdate(
         address owner,
-        string username,
+        string nickname,
         string name,
         string country,
         DecentravellerDataTypes.DecentravellerGender gender,
-        string interest
+        DecentravellerDataTypes.DecentravellerInterest interest
     );
 
     uint256 private currentPlaceId;
     DecentravellerPlaceCloneFactory placeFactory;
     mapping(address => DecentravellerDataTypes.DecentravellerProfile) profilesByowner;
+    mapping(string => address) ownersByNicknames;
+
+    function registerProfile(
+        string memory _nickname,
+        string memory _name,
+        string memory _country,
+        DecentravellerDataTypes.DecentravellerGender _gender,
+        DecentravellerDataTypes.DecentravellerInterest _interest
+    ) public returns (address owner) {
+        if(ownersByNicknames[_nickname] != address(0) && ownersByNicknames[_nickname] != msg.sender){
+            revert Profile__NicknameInUse(_nickname);
+        }
+        if(profilesByowner[msg.sender].owner != address(0)){
+            // Nickname change
+            delete ownersByNicknames[profilesByowner[msg.sender].nickname];
+        }
+        ownersByNicknames[_nickname] = msg.sender;
+        profilesByowner[msg.sender].owner = msg.sender;
+        profilesByowner[msg.sender].nickname = _nickname;
+        profilesByowner[msg.sender].name = _name;
+        profilesByowner[msg.sender].country = _country;
+        profilesByowner[msg.sender].gender = _gender;
+        profilesByowner[msg.sender].interest = _interest;
+
+        emit ProfileUpdate(
+            msg.sender,
+            _nickname,
+            _name,
+            _country,
+            _gender,
+            _interest
+        );
+
+        return msg.sender;
+    }
 
     constructor(address _placesFactory) {
         placeFactory = DecentravellerPlaceCloneFactory(_placesFactory);
@@ -36,6 +73,7 @@ contract Decentraveller {
         string memory _physicalAddress,
         DecentravellerDataTypes.DecentravellerPlaceCategory category
     ) public returns (uint256 placeId) {
+        if(profilesByowner[msg.sender].owner == address(0)) revert Address__Unregistered(msg.sender);
         currentPlaceId += 1;
 
         placeAddressByPlaceId[currentPlaceId] = placeFactory.createNewPlace(
