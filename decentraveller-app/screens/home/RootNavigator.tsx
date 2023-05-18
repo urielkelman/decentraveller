0a53e969e6a8547a152c82cdaefc8fcc97b2b0ea
Uriel Kelman <uriel.k@lemon.me>
2023-05-18 00:05:17 -0300

    backup commit

--- decentraveller-app/screens/home/RootNavigator.tsx
+++ decentraveller-app/screens/home/RootNavigator.tsx
@@ -18,42 +18,47 @@ type bottomTabElementProps = {
     size: number;
 };
 
-const getTabBarIcon = (
-    route: Readonly<{ key: string; name: 'ConnectWalletScreen' | 'Home'; path?: string }>,
-    iconSize: number
-) => {
-    return ({ focused, color, size }: bottomTabElementProps): JSX.Element => {
-        if (route.name === 'ConnectWalletScreen') {
-            return <AntDesign name="home" size={iconSize} color="black" />;
-        } else if (route.name === 'Home') {
-            return <AntDesign name="user" size={iconSize} color="black" />;
-        }
-        return <AntDesign name="home" size={iconSize} color="black" />;
-    };
-};
-
-const getBottomTabNavigatorScreenOptions = (
-    route: Readonly<{ key: string; name: 'ConnectWalletScreen' | 'Home'; path?: string }>
-): BottomTabNavigationOptions => {
-    const deviceDimensions = useDeviceDimensions();
-    return {
-        tabBarIcon: getTabBarIcon(route, deviceDimensions.width / sizesConfig.bottomTabIconSizeWidthInverseFraction),
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-        tabBarStyle: {
-            height: deviceDimensions.height / sizesConfig.bottomTabHeightInverseFraction,
-        },
-        tabBarLabelStyle: {
-            fontSize: deviceDimensions.width / sizesConfig.bottomTabFontSizeWidthInverseFraction,
-            fontFamily: 'sans-serif-medium',
-            fontWeight: 'bold',
-        },
-        title: route.name.replace('Navigator', ''),
-        headerShown: false,
-    };
-};
+const DEFAULT_BOTTOM_TAB_ICONS_COLOR = "#FFE1E1"
 
 const RootNavigator = () => {
+
+    const getTabBarIcon = (
+        route: Readonly<{ key: string; name: 'ConnectWalletScreen' | 'Home'; path?: string }>,
+        iconSize: number
+    ) => {
+        return ({ focused, color, size }: bottomTabElementProps): JSX.Element => {
+            if (route.name === 'ConnectWalletScreen') {
+                return <AntDesign name="home" size={iconSize} color={DEFAULT_BOTTOM_TAB_ICONS_COLOR} />;
+            } else if (route.name === 'Home') {
+                console.log('asd')
+                return <AntDesign name="user" size={iconSize} color={DEFAULT_BOTTOM_TAB_ICONS_COLOR} />;
+            }
+            return <AntDesign name="home" size={iconSize} color={DEFAULT_BOTTOM_TAB_ICONS_COLOR} />;
+        };
+    };
+
+    const getBottomTabNavigatorScreenOptions = (
+        route: Readonly<{ key: string; name: 'Home'; path?: string }>
+    ): BottomTabNavigationOptions => {
+        const deviceDimensions = useDeviceDimensions();
+        console.log(route)
+        return {
+            tabBarIcon: getTabBarIcon(route, deviceDimensions.width / sizesConfig.bottomTabIconSizeWidthInverseFraction),
+            tabBarActiveTintColor: 'black',
+            tabBarInactiveTintColor: 'gray',
+            tabBarStyle: {
+                height: deviceDimensions.height / sizesConfig.bottomTabHeightInverseFraction,
+            },
+            tabBarLabelStyle: {
+                fontSize: deviceDimensions.width / sizesConfig.bottomTabFontSizeWidthInverseFraction,
+                fontFamily: 'sans-serif-medium',
+                fontWeight: 'bold',
+            },
+            title: route.name.replace('Navigator', ''),
+            headerShown: false,
+        };
+    };
+
     return (
         <RootTabNavigator.Navigator
             initialRouteName="Home"
