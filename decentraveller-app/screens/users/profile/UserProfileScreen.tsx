ca79d137cc8531bf5d51505c36c4b4e50186bf4e
Matias Scakosky <matias.scakosky@pomelo.la>
2023-05-27 18:46:21 -0300

    add styles

--- decentraveller-app/screens/users/profile/UserProfileScreen.tsx
+++ decentraveller-app/screens/users/profile/UserProfileScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, Image, StyleSheet} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import {userProfileMainStyles} from "../../../styles/userProfileStyles";
 
 export type UserProfileScreens = {
     UserProfileScreen: undefined;
@@ -16,144 +17,54 @@ const UserProfileScreen = ({ navigation }) => {
     return (
         <View style={{ flex: 1, backgroundColor: '#FFE1E1' }}>
             {/*RECUADRO DE USER*/}
-            <View style={styles.mainContainer}>
-                <View style={styles.imageContainer}>
-                    <View style={styles.imageCircle}>
-                        <Image source={require('../../../assets/mock_images/cryptochica.png')} style={styles.circleDimensions} />
+            <View style={userProfileMainStyles.mainContainer}>
+                <View style={userProfileMainStyles.imageContainer}>
+                    <View style={userProfileMainStyles.imageCircle}>
+                        <Image source={require('../../../assets/mock_images/cryptochica.png')} style={userProfileMainStyles.circleDimensions} />
                     </View>
                 </View>
-                <View style={styles.titleContainer}>
-                    <Text style={styles.nicknameTitle}>ElUriK</Text>
-                    <Text style={styles.walletSubtitle}>0x3FZbgi29cpjq2GjdwV8eyHuJJnkLtktZc5</Text>
+                <View style={userProfileMainStyles.titleContainer}>
+                    <Text style={userProfileMainStyles.nicknameTitle}>ElUriK</Text>
+                    <Text style={userProfileMainStyles.walletSubtitle}>0x3FZbgi29cpjq2GjdwV8eyHuJJnkLtktZc5</Text>
                 </View>
-                <View style={styles.joinedAtContainer}>
-                    <Text style={styles.joinedAtText}>Joined Decentraveller on: 10/05/2023</Text>
+                <View style={userProfileMainStyles.joinedAtContainer}>
+                    <Text style={userProfileMainStyles.joinedAtText}>Joined Decentraveller on: 10/05/2023</Text>
                 </View>
             </View>
 
-            <View style={styles.informationContainer}>
+            <View style={userProfileMainStyles.informationContainer}>
                 <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, marginTop: 10 }}>
-                    <Text style={styles.leftText}>Main interest</Text>
-                    <Text style={styles.rightText}>Gastronomy</Text>
+                    <Text style={userProfileMainStyles.leftText}>Main interest</Text>
+                    <Text style={userProfileMainStyles.rightText}>Gastronomy</Text>
                 </View>
-                <View style={styles.spacedBetweenView}>
-                    <Text style={styles.leftText}>Shared Location</Text>
-                    <Text style={styles.rightText}>Yes</Text>
+                <View style={userProfileMainStyles.spacedBetweenView}>
+                    <Text style={userProfileMainStyles.leftText}>Shared Location</Text>
+                    <Text style={userProfileMainStyles.rightText}>Yes</Text>
                 </View>
-                <View style={styles.spacedBetweenView}   >
-                    <Text style={styles.leftText}>DT Tokens</Text>
-                    <Text style={styles.rightText}>67 </Text>
-                </View>
-            </View>
-
-            <View style={styles.informationContainer}>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, marginTop: 40 }}   >
-                    <Text style={styles.leftText}>My Places(3)</Text>
-                    <Text style={styles.rightBlueText}>More </Text>
+                <View style={userProfileMainStyles.spacedBetweenView}   >
+                    <Text style={userProfileMainStyles.leftText}>DT Tokens</Text>
+                    <Text style={userProfileMainStyles.rightText}>67 </Text>
                 </View>
             </View>
 
-
-            <View style={styles.informationContainer}>
+            <View style={userProfileMainStyles.informationContainer}>
                 <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, marginTop: 40 }}   >
-                    <Text style={styles.leftText}>My Reviews(5)</Text>
-                    <Text style={styles.rightBlueText}>More </Text>
+                    <Text style={userProfileMainStyles.leftText}>My Places(3)</Text>
+                    <Text style={userProfileMainStyles.rightBlueText}>More </Text>
+                </View>
+            </View>
+
+
+            <View style={userProfileMainStyles.informationContainer}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, marginTop: 40 }}   >
+                    <Text style={userProfileMainStyles.leftText}>My Reviews(5)</Text>
+                    <Text style={userProfileMainStyles.rightBlueText}>More </Text>
                 </View>
             </View>
 
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    mainContainer: {
-        backgroundColor: '#FFFFFF',
-        borderRadius: 10,
-        marginHorizontal: 13,
-        marginTop: 20,
-        width: 388,
-        height: 247,
-    },
-    imageContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 24
-    },
-    imageCircle: {
-        backgroundColor: 'lightgray',
-        borderRadius: 50,
-        width: 100,
-        height: 100,
-        overflow: 'hidden'
-    },
-    circleDimensions: {
-        width: 100,
-        height: 100
-    },
-    titleContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 10
-    },
-    nicknameTitle: {
-        fontFamily: 'Montserrat_700Bold',
-        fontSize: 18,
-        textAlign: 'center'
-    },
-    walletSubtitle: {
-        fontFamily: 'Montserrat_500Medium',
-        fontSize: 11,
-        textAlign: 'center'
-    },
-    joinedAtContainer: {
-        backgroundColor: '#F3F3F3',
-        borderRadius: 5,
-        marginHorizontal: 14,
-        marginTop: 15,
-        height: 41,
-        justifyContent: 'center'
-    },
-    joinedAtText: {
-        fontFamily: 'Montserrat_500Medium',
-        fontSize: 11,
-        color: '#020202',
-        paddingHorizontal: 10
-    },
-    informationContainer: {
-        backgroundColor: '#FFFFFF',
-        borderRadius: 10,
-        marginHorizontal: 13,
-        marginTop: 10,
-        width: 388,
-        height: 100
-    },
-    spacedBetweenView: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 10,
-        marginTop: 10
-    },
-    leftText: {
-        fontFamily: 'Montserrat_700Bold',
-        fontSize: 15,
-        textAlign: 'left',
-        fontWeight: 'bold'
-    },
-    rightText: {
-        fontFamily: 'Montserrat_500Medium', 
-        fontSize: 15, 
-        textAlign: 'right', 
-        fontWeight: '100'
-    },
-    rightBlueText: {
-        fontFamily: 'Montserrat_500Medium',
-        fontSize: 15,
-        textAlign: 'right',
-        fontWeight: 'bold',
-        color: 'blue'
-    },
-
-});
 
 const UserNavigator = () => {
     return (
