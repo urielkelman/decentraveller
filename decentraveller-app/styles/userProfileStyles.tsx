674699a31ca6b96ef3f2934062cd57c4157dab4c
mscakosky-pm <104372455+mscakosky-pm@users.noreply.github.com>
2023-05-28 20:36:11 -0300

    User profile (#19)
    * add user profile main screen
    
    * add styles
    
    * use context to fill profile data
    
    * add my places views with api adapter and placeholder of reviews
    
    * linter fixes
    
    * review changes

---
+++ decentraveller-app/styles/userProfileStyles.tsx
@@ -0,0 +1,102 @@
+import { StyleSheet } from 'react-native';
+
+const userProfileMainStyles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: '#FFE1E1',
+    },
+    mainContainer: {
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        marginHorizontal: 13,
+        marginTop: 20,
+        width: 388,
+        height: 247,
+    },
+    imageContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 24,
+    },
+    imageCircle: {
+        backgroundColor: 'lightgray',
+        borderRadius: 50,
+        width: 100,
+        height: 100,
+        overflow: 'hidden',
+    },
+    circleDimensions: {
+        width: 100,
+        height: 100,
+    },
+    titleContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+    },
+    nicknameTitle: {
+        fontFamily: 'Montserrat_700Bold',
+        fontSize: 18,
+        textAlign: 'center',
+    },
+    walletSubtitle: {
+        fontFamily: 'Montserrat_500Medium',
+        fontSize: 11,
+        textAlign: 'center',
+    },
+    joinedAtContainer: {
+        backgroundColor: '#F3F3F3',
+        borderRadius: 5,
+        marginHorizontal: 14,
+        marginTop: 15,
+        height: 41,
+        justifyContent: 'center',
+    },
+    joinedAtText: {
+        fontFamily: 'Montserrat_500Medium',
+        fontSize: 11,
+        color: '#020202',
+        paddingHorizontal: 10,
+    },
+    informationContainer: {
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        marginHorizontal: 13,
+        marginTop: 10,
+        width: 388,
+        height: 100,
+    },
+    spacedBetweenView: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 10,
+        marginTop: 10,
+    },
+    spacedButtonBetweenView: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 10,
+        marginTop: 40,
+    },
+    leftText: {
+        fontFamily: 'Montserrat_700Bold',
+        fontSize: 15,
+        textAlign: 'left',
+        fontWeight: 'bold',
+    },
+    rightText: {
+        fontFamily: 'Montserrat_500Medium',
+        fontSize: 15,
+        textAlign: 'right',
+        fontWeight: '100',
+    },
+    rightBlueText: {
+        fontFamily: 'Montserrat_500Medium',
+        fontSize: 15,
+        textAlign: 'right',
+        fontWeight: 'bold',
+        color: 'blue',
+    },
+});
+
+export { userProfileMainStyles };
