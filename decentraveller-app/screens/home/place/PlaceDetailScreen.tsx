41634d52ef25068e334fa2c371cdcae56c877e6a
Matias Scakosky <matias.scakosky@pomelo.la>
2023-05-30 11:12:53 -0300

    add image on place detail

--- decentraveller-app/screens/home/place/PlaceDetailScreen.tsx
+++ decentraveller-app/screens/home/place/PlaceDetailScreen.tsx
@@ -1,15 +1,50 @@
-import { View, Text } from 'react-native';
+import {View, Image, StyleSheet, Dimensions} from 'react-native';
 
-export type PlaceDetailScreen = {
-    Place: undefined;
-};
 
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
+const path = '../../../assets/mock_images/eretz-inside.jpeg'
 const PlaceDetailScreen = () => {
     return (
-        <View>
-            <Text>Place detail</Text>
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={require(path)}
+                    style={styles.image}
+                >
+                    {/* Aquí puedes agregar otros elementos o contenido dentro de la imagen */}
+                </Image>
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFE1E1',
+
+    },
+    image: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+    imageContainer: {
+        borderRadius: 45,
+        transform: [{ translateY: -60 }],
+        marginTop: 10,
+        justifyContent: 'flex-end',
+        overflow: 'hidden',
+        height:windowHeight * 0.50,
+        width:windowWidth,
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity: 10,
+        shadowRadius: 0.5,
+        shadowColor: '#000',
+        elevation: 10,
+    },
+});
 
 export default PlaceDetailScreen;
