de2ecbdde5d731d5aaa47c0e684855954b541791
Matias Scakosky <matias.scakosky@pomelo.la>
2023-05-31 21:16:13 -0300

    finish image

--- decentraveller-app/screens/home/place/PlaceDetailScreen.tsx
+++ decentraveller-app/screens/home/place/PlaceDetailScreen.tsx
@@ -31,6 +31,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         resizeMode: 'cover',
+        marginTop: -20
     },
     imageContainer: {
         borderRadius: 45,
