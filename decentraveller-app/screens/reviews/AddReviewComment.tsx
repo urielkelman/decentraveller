5367607c16523a61c339bf8b8c0292c046157f72
Matias Scakosky <matias.scakosky@pomelo.la>
2023-06-13 23:30:36 -0300

    split review styles file

--- decentraveller-app/screens/reviews/AddReviewComment.tsx
+++ decentraveller-app/screens/reviews/AddReviewComment.tsx
@@ -3,6 +3,7 @@ import {View, StyleSheet, Image, Text, TouchableOpacity, TextInput} from 'react-
 import { Ionicons } from '@expo/vector-icons';
 import {addReviewsScreenWordings} from "./wording";
 import DecentravellerButton from "../../commons/components/DecentravellerButton";
+import {addReviewCommentStyles} from "../../styles/addReviewStyles";
 
 const imagePath1 = '../../assets/images/ar4.jpeg'
 
@@ -37,22 +38,22 @@ const AddReviewComment = () => {
     };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.commentTitle}>{addReviewsScreenWordings.ADD_COMMENT_TITLE}</Text>
-            <View style={styles.commentExampleContainer}>
-                <Image source={require('../../assets/images/example-girl.jpeg')} style={styles.imageUserExample} />
-                <View style={styles.dialogContainer}>
+        <View style={addReviewCommentStyles.container}>
+            <Text style={addReviewCommentStyles.commentTitle}>{addReviewsScreenWordings.ADD_COMMENT_TITLE}</Text>
+            <View style={addReviewCommentStyles.commentExampleContainer}>
+                <Image source={require('../../assets/images/example-girl.jpeg')} style={addReviewCommentStyles.imageUserExample} />
+                <View style={addReviewCommentStyles.dialogContainer}>
                     <Image source={require('../../assets/images/dialog.png')}/>
-                    <Text style={styles.dialogText}>{addReviewsScreenWordings.EXAMPLE_COMMENT_REVIEW}</Text>
+                    <Text style={addReviewCommentStyles.dialogText}>{addReviewsScreenWordings.EXAMPLE_COMMENT_REVIEW}</Text>
                 </View>
             </View>
 
-            <Text style={styles.commentSubtitle}>{addReviewsScreenWordings.ADD_RATE}</Text>
-            <View style={styles.starContainer}>{renderStars()}</View>
-            <Text style={styles.commentSubtitle}>{addReviewsScreenWordings.ADD_COMMENT_SUBTITLE}</Text>
-            <View style={styles.inputContainer}>
+            <Text style={addReviewCommentStyles.commentSubtitle}>{addReviewsScreenWordings.ADD_RATE}</Text>
+            <View style={addReviewCommentStyles.starContainer}>{renderStars()}</View>
+            <Text style={addReviewCommentStyles.commentSubtitle}>{addReviewsScreenWordings.ADD_COMMENT_SUBTITLE}</Text>
+            <View style={addReviewCommentStyles.inputContainer}>
                 <TextInput
-                    style={styles.textInput}
+                    style={addReviewCommentStyles.textInput}
                     multiline
                     placeholder="Insert comment here"
                     placeholderTextColor="gray"
@@ -69,86 +70,5 @@ const AddReviewComment = () => {
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFE1E1',
-        padding: 25,
-    },
-    commentExampleContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: 20,
-        marginHorizontal: 30,
-        marginLeft: -10
-    },
-
-    imageUserExample: {
-        width: 50,
-        height: 50,
-        borderRadius: 50,
-        marginLeft: 25,
-        marginTop: 20,
-    },
-    commentTitle: {
-        fontSize: 15,
-        fontFamily: 'Montserrat_700Bold',
-        textAlign: 'left',
-        marginRight: 10,
-        marginLeft: 10,
-    },
-    commentSubtitle: {
-        fontSize: 15,
-        fontFamily: 'Montserrat_700Bold',
-        textAlign: 'left',
-        marginTop: 10,
-        marginRight: 10,
-        marginLeft: 10,
-    },
-    dialogText: {
-        position: 'absolute',
-        top: '20%',
-        color: 'gray',
-        fontSize: 16,
-        textAlign: 'center',
-        paddingHorizontal: 20,
-    },
-    dialogContainer: {
-        alignItems: 'center',
-        marginLeft: -10
-    },
-    inputContainer: {
-        paddingHorizontal: 10,
-        paddingVertical: 5,
-        marginTop: 10,
-        borderColor: 'white',
-        borderWidth: 1,
-        borderRadius: 5,
-        minHeight: 200,
-        backgroundColor: 'white',
-    },
-    textInput: {
-        flex: 1,
-        fontSize: 16,
-        color: 'black',
-        position: 'absolute',
-        top: 10,
-        left: 10,
-    },
-    placeholderText: {
-        position: 'absolute',
-        top: 10,
-        left: 10,
-        fontSize: 16,
-        color: 'gray',
-    },
-    starContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop: 5,
-    },
-});
-
 
 export default AddReviewComment;
