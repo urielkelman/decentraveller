c1c12388f371a72dddd9635252ce78911ab3f64d
Matias Scakosky <matias.scakosky@pomelo.la>
2023-06-13 23:30:36 -0300

    split review styles file

--- decentraveller-app/screens/reviews/AddReviewImages.tsx
+++ decentraveller-app/screens/reviews/AddReviewImages.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, StyleSheet, Image, Text, TouchableOpacity} from 'react-native';
 import {addReviewsScreenWordings} from "./wording";
 import DecentravellerButton from "../../commons/components/DecentravellerButton";
+import {addReviewImagesStyles} from "../../styles/addReviewStyles";
 
 const imagePath1 = '../../assets/images/ar4.jpeg'
 const imagePath2 = '../../assets/images/ar2.jpeg'
@@ -14,15 +15,15 @@ type CircleImageItemProps = {
     imagePath: any;
 };
 const circleImage: React.FC<CircleImageItemProps> = ({imagePath}) => {
-    return ( <View style={styles.circleImage}>
-        <Image source={imagePath} style={styles.image} />
+    return ( <View style={addReviewImagesStyles.circleImage}>
+        <Image source={imagePath} style={addReviewImagesStyles.image} />
     </View> )
 }
 
 const AddReviewImages = ({navigation}) => {
     return (
-        <View style={styles.container}>
-            <View style={styles.commentExampleContainer}>
+        <View style={addReviewImagesStyles.container}>
+            <View style={addReviewImagesStyles.commentExampleContainer}>
                 {circleImage({ imagePath: require(imagePath1) })}
                 {circleImage({ imagePath: require(imagePath2) })}
                 {circleImage({ imagePath: require(imagePath3) })}
@@ -30,12 +31,12 @@ const AddReviewImages = ({navigation}) => {
                 {circleImage({ imagePath: require(imagePath5) })}
                 {circleImage({ imagePath: require(imagePath6) })}
             </View>
-            <Text style={styles.title}>{addReviewsScreenWordings.ADD_IMAGE_TITLE}</Text>
-            <View style={styles.uploadContainer}>
-                <View style={styles.circleImage}>
-                    <Image source={require('../../assets/images/imageUpload.jpeg')} style={styles.imageUpload} />
-                    <TouchableOpacity style={styles.smallCircleButton}>
-                        <Image source={require('../../assets/images/pencil.png')} style={styles.smallCircleImage} />
+            <Text style={addReviewImagesStyles.title}>{addReviewsScreenWordings.ADD_IMAGE_TITLE}</Text>
+            <View style={addReviewImagesStyles.uploadContainer}>
+                <View style={addReviewImagesStyles.circleImage}>
+                    <Image source={require('../../assets/images/imageUpload.jpeg')} style={addReviewImagesStyles.imageUpload} />
+                    <TouchableOpacity style={addReviewImagesStyles.smallCircleButton}>
+                        <Image source={require('../../assets/images/pencil.png')} style={addReviewImagesStyles.smallCircleImage} />
                     </TouchableOpacity>
                 </View>
             </View>
@@ -43,72 +44,5 @@ const AddReviewImages = ({navigation}) => {
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFE1E1',
-        padding: 25,
-    },
-    commentExampleContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: 20,
-        marginHorizontal: -5,
-    },
-    circleImage: {
-        width: 50,
-        height: 50,
-        borderRadius: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    uploadContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    circleUpload: {
-        width: 200,
-        height: 200,
-        borderRadius: 100,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    imageUpload: {
-        width: 180,
-        height: 180,
-        borderRadius: 90,
-    },
-    image: {
-        width: 60,
-        height: 60,
-        borderRadius: 50,
-    },
-    smallCircleButton: {
-        position: 'absolute',
-        top: -70,
-        right: -60,
-        width: 45,
-        height: 45,
-        borderRadius: 100,
-        backgroundColor: '#983B46',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    smallCircleImage: {
-        width: 24,
-        height: 24,
-    },
-    title: {
-        fontSize: 20,
-        fontFamily: 'Montserrat_700Bold',
-        textAlign: 'center',
-        marginRight: 10,
-        marginLeft: 10,
-        marginTop: 50,
-    },
-});
-
 
 export default AddReviewImages;
