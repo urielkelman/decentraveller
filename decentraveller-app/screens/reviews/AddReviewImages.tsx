1353393cf7ab73e316a4736fcd7de2ca16494f8d
Matias Scakosky <matias.scakosky@pomelo.la>
2023-06-13 23:32:05 -0300

    linter fixes

--- decentraveller-app/screens/reviews/AddReviewImages.tsx
+++ decentraveller-app/screens/reviews/AddReviewImages.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import {View, StyleSheet, Image, Text, TouchableOpacity} from 'react-native';
-import {addReviewsScreenWordings} from "./wording";
-import DecentravellerButton from "../../commons/components/DecentravellerButton";
-import {addReviewImagesStyles} from "../../styles/addReviewStyles";
+import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
+import { addReviewsScreenWordings } from './wording';
+import DecentravellerButton from '../../commons/components/DecentravellerButton';
+import { addReviewImagesStyles } from '../../styles/addReviewStyles';
 
-const imagePath1 = '../../assets/images/ar4.jpeg'
-const imagePath2 = '../../assets/images/ar2.jpeg'
-const imagePath3 = '../../assets/images/ar1.jpeg'
-const imagePath4 = '../../assets/images/ar3.jpeg'
-const imagePath5 = '../../assets/images/ar5.jpeg'
-const imagePath6 = '../../assets/images/ar6.jpeg'
+const imagePath1 = '../../assets/images/ar4.jpeg';
+const imagePath2 = '../../assets/images/ar2.jpeg';
+const imagePath3 = '../../assets/images/ar1.jpeg';
+const imagePath4 = '../../assets/images/ar3.jpeg';
+const imagePath5 = '../../assets/images/ar5.jpeg';
+const imagePath6 = '../../assets/images/ar6.jpeg';
 
 type CircleImageItemProps = {
     imagePath: any;
 };
-const circleImage: React.FC<CircleImageItemProps> = ({imagePath}) => {
-    return ( <View style={addReviewImagesStyles.circleImage}>
-        <Image source={imagePath} style={addReviewImagesStyles.image} />
-    </View> )
-}
+const circleImage: React.FC<CircleImageItemProps> = ({ imagePath }) => {
+    return (
+        <View style={addReviewImagesStyles.circleImage}>
+            <Image source={imagePath} style={addReviewImagesStyles.image} />
+        </View>
+    );
+};
 
-const AddReviewImages = ({navigation}) => {
+const AddReviewImages = ({ navigation }) => {
     return (
         <View style={addReviewImagesStyles.container}>
             <View style={addReviewImagesStyles.commentExampleContainer}>
@@ -34,13 +36,25 @@ const AddReviewImages = ({navigation}) => {
             <Text style={addReviewImagesStyles.title}>{addReviewsScreenWordings.ADD_IMAGE_TITLE}</Text>
             <View style={addReviewImagesStyles.uploadContainer}>
                 <View style={addReviewImagesStyles.circleImage}>
-                    <Image source={require('../../assets/images/imageUpload.jpeg')} style={addReviewImagesStyles.imageUpload} />
+                    <Image
+                        source={require('../../assets/images/imageUpload.jpeg')}
+                        style={addReviewImagesStyles.imageUpload}
+                    />
                     <TouchableOpacity style={addReviewImagesStyles.smallCircleButton}>
-                        <Image source={require('../../assets/images/pencil.png')} style={addReviewImagesStyles.smallCircleImage} />
+                        <Image
+                            source={require('../../assets/images/pencil.png')}
+                            style={addReviewImagesStyles.smallCircleImage}
+                        />
                     </TouchableOpacity>
                 </View>
             </View>
-            <DecentravellerButton text={'Next'} loading={false} onPress={()=>{navigation.navigate('AddReviewComment')}} />
+            <DecentravellerButton
+                text={'Next'}
+                loading={false}
+                onPress={() => {
+                    navigation.navigate('AddReviewComment');
+                }}
+            />
         </View>
     );
 };
