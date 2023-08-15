e383bd1f29e1e30b7baa164aeb20c0299bef3959
Uriel Kelman <uriel.k@lemon.me>
2023-08-15 01:53:54 -0300

    started governance

--- decentraveller-blockchain/contracts/DecentravellerGovernance.sol
+++ decentraveller-blockchain/contracts/DecentravellerGovernance.sol
@@ -6,4 +6,18 @@ import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol
 
 contract DecentravellerGovernance is GovernorCountingSimple {
     constructor(string memory name) Governor(name) {}
+
+    function votingDelay() public view virtual override returns (uint256) {}
+
+    function votingPeriod() public view virtual override returns (uint256) {}
+
+    function quorum(
+        uint256 blockNumber
+    ) public view virtual override returns (uint256) {}
+
+    function _getVotes(
+        address account,
+        uint256 blockNumber,
+        bytes memory params
+    ) internal view virtual override returns (uint256) {}
 }
