f93b3fec97c9e2955ed41c4a39fa965db3289ff5
Matias Scakosky <matias.scakosky@pomelo.la>
2023-09-27 14:13:14 -0300

    add wording

---
+++ decentraveller-app/screens/home/community/wording.tsx
@@ -0,0 +1,20 @@
+export const communityWording = {
+    PROPOSE_DELETE: 'Propose to delete',
+    ENQUEUE: 'Enqueue',
+    EXECUTE: 'Execute',
+    GO_BACK: 'Go back',
+    SUCCEDED_VOTATION: 'This rule passed the votation, queue it to make it part of the community',
+    QUEUED_VOTATION: 'Execute this rule in order to finish de proposal flow',
+    DEFEATED_VOTATION: 'This rule has been defeated, the community don\'t want this rule, there is no action possible',
+    PENDING_VOTATION: 'The votation is not available at this moment, wait.',
+    ACTIVE_VOTATION: 'Vote in favor or against to approve rule.',
+    APPROVED_STATUS: 'The following rule is now part of the community',
+    PROPOSE_DELETE_ACTIONABLE: 'If you don\'t agree, you can propose removing it.',
+    DELETED_STATUS: 'The following rule is not part of the community',
+    NO_ACTION_AVAILABLE: 'The following rule is not part of the community',
+    PENDING_APPROVAL_STATUS: 'The following rule is pending approval',
+    PENDING_DELETED_STATUS: 'The following rule is pending to delete',
+    ACCEPTED_RULES: 'Those rules are accepted by the community and all members should keep them.',
+    VOTATION_RULES: 'Rules that are not part of the community yet, you can chose the status and take different actions to make it part',
+
+};
